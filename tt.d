cc912162095a9f61a26394d2f819c1d56d77bd78
drue <drue@gigagig.org>
2008-04-11 04:34:42 -0700

    test file

---
+++ tt.d
@@ -0,0 +1,24 @@
+module tt;
+
+import tango.net.InternetAddress;
+
+import tangled.interfaces;
+import tangled.protocol;
+import tangled.reactor;
+
+int main() {
+  reactor.tcpListen(new InternetAddress("127.0.0.1", 6060), new SimpleFactory!(Echo)());
+  reactor.run();
+  return 0;
+}
+
+class Echo : BaseProtocol {
+  void makeConnection(IAConduit transport) {
+    char[] buf;
+    transport.write("Welcome to the Echo Server\n");
+    while(1) {
+      transport.read(buf);
+      transport.write(buf);
+    }
+  }
+}
